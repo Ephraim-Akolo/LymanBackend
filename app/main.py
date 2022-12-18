ccc870c31ddcffd4a7e3ecf7db1b26cd5466470b
jake-ephraim <ephraimakolo2017@gmail.com>
2022-12-18 16:16:03 +0100

    update

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from .database import engine, Base
 from .routers import artistans, customers, products, auth
@@ -15,3 +16,5 @@ app.include_router(customers.router)
 app.include_router(artistans.router)
 app.include_router(products.router)
 
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
