264f1490336b588c6b982aa4c48f8aac5553f721
jake-ephraim <ephraimakolo2017@gmail.com>
2022-12-19 12:23:46 +0100

    added environmental vairables auto import

--- app/utils.py
+++ app/utils.py
@@ -8,3 +8,19 @@ def hash(password:str):
 
 def verify(plain_password:str, hashed_password:str):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    elyman_database_user:str
+    elyman_database_name:str
+    elyman_database_server:str = "database-2.c3egd4km892g.us-east-1.rds.amazonaws.com"
+    elyman_database_password:str = 'akolo000'
+    elyman_secret_key:str = "d329748e4dce05b3fcf429da2bb8914f240ff0c288897f455ae792f82aee3cc0"
+    elyman_algorithm:str = "HS256"
+    elyman_access_token_expire_minutes:int
+    class Config:
+        env_file = "./app/.env"
+
+settings = Settings()
